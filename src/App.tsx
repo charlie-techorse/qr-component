680ca065af7804c059ad7a7a05a4b83104ce6474
Charlie Hanekamp <charlie@winkwaves.com>
2023-02-04 16:38:57 +0100

    Add basic panel to the app

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,8 @@
-import { useState } from 'react'
-import './App.css'
+import { Panel } from './components/Panel'
 
 function App() {
-
   return (
-    <div className="App">
-
-    </div>
+    <Panel />
   )
 }
 
---
+++ src/components/Panel.tsx
@@ -0,0 +1,7 @@
+import styles from './Panel.module.css'
+
+export function Panel() {
+  return <div className={styles.panel}>
+
+  </div>
+}
