15b3d353d42777ece941e86dbfc696db3b891b23
Charlie Hanekamp <charlie@winkwaves.com>
2023-02-04 16:49:07 +0100

    Add the title

--- src/components/Panel.tsx
+++ src/components/Panel.tsx
@@ -3,5 +3,8 @@ import styles from './Panel.module.css'
 export function Panel() {
   return <div className={styles.panel}>
     <img className={styles.qr} src='/image-qr-code.png'></img>
+    <div className={styles.text}>
+      <h1 className={styles.title}>Improve your front-end skills by building projects</h1>
+    </div>
   </div>
 }
