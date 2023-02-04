b0fc3ff01290751a42a191523818a8cf44266365
Charlie Hanekamp <charlie@winkwaves.com>
2023-02-04 16:51:16 +0100

    Add the text to the panel

--- src/components/Panel.tsx
+++ src/components/Panel.tsx
@@ -5,6 +5,7 @@ export function Panel() {
     <img className={styles.qr} src='/image-qr-code.png'></img>
     <div className={styles.text}>
       <h1 className={styles.title}>Improve your front-end skills by building projects</h1>
+      <p className={styles.paragraph}>Scan the QR code to visit Frontend Mentor and take your coding skills to the next level</p>
     </div>
   </div>
 }
