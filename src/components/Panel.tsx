075e8f6548fe4388e548b39290ee31ce45e480f8
Charlie Hanekamp <charlie@winkwaves.com>
2023-02-04 17:28:30 +0100

    import the correct css file

--- src/components/Panel.tsx
+++ src/components/Panel.tsx
@@ -1,11 +1,11 @@
-import styles from './Panel.module.css'
+import './panel.css'
 
 export function Panel() {
-  return <div className={styles.panel}>
-    <img className={styles.qr} src='/image-qr-code.png'></img>
-    <div className={styles.text}>
-      <h1 className={styles.title}>Improve your front-end skills by building projects</h1>
-      <p className={styles.paragraph}>Scan the QR code to visit Frontend Mentor and take your coding skills to the next level</p>
+  return <div className='panel'>
+    <img className='panel__img' src='/image-qr-code.png'></img>
+    <div className='panel__text'>
+      <h1 className='panel__title'>Improve your front-end skills by building projects</h1>
+      <p className='panel__paragraph'>Scan the QR code to visit Frontend Mentor and take your coding skills to the next level</p>
     </div>
   </div>
 }
