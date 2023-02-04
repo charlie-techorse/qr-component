50cf729cbfcdcd6479aaa32384a52ae39fce7108
Charlie Hanekamp <charlie@winkwaves.com>
2023-02-04 16:45:11 +0100

    Add QR image

--- src/components/Panel.tsx
+++ src/components/Panel.tsx
@@ -2,6 +2,6 @@ import styles from './Panel.module.css'
 
 export function Panel() {
   return <div className={styles.panel}>
-
+    <img className={styles.qr} src='/image-qr-code.png'></img>
   </div>
 }
