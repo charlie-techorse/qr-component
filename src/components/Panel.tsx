6be04869f60249b28d5edb4cb6dbb5d2fd5f163e
Charlie Hanekamp <charlie@winkwaves.com>
2023-02-10 08:45:49 +0100

    Fix accessibility errors

--- src/components/Panel.tsx
+++ src/components/Panel.tsx
@@ -2,7 +2,7 @@ import './panel.css'
 
 export function Panel() {
   return <div className='panel'>
-    <img className='panel__img' src='/image-qr-code.png'></img>
+    <img className='panel__img' alt="qr-code" src='/image-qr-code.png'></img>
     <div className='panel__text'>
       <h1 className='panel__title'>Improve your front-end skills by building projects</h1>
       <p className='panel__paragraph'>Scan the QR code to visit Frontend Mentor and take your coding skills to the next level</p>
